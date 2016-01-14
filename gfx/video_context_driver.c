19464d001d5199302e2bcbf3dc4197e3b1f46c5e
k1ll <karl.glatzer@gmx.de>
2016-01-14 14:04:51 +0100

    Wayland context can be used with both OpenGL and OpenGLES

--- gfx/video_context_driver.c
+++ gfx/video_context_driver.c
@@ -49,7 +49,7 @@ static const gfx_ctx_driver_t *gfx_ctx_drivers[] = {
 #if defined(HAVE_X11) && defined(HAVE_OPENGL) && !defined(HAVE_OPENGLES)
    &gfx_ctx_glx,
 #endif
-#if defined(HAVE_WAYLAND) && defined(HAVE_OPENGL) && defined(HAVE_EGL)
+#if defined(HAVE_WAYLAND) && (defined(HAVE_OPENGL) || defined(HAVE_OPENGLES)) && defined(HAVE_EGL)
    &gfx_ctx_wayland,
 #endif
 #if defined(HAVE_X11) && defined(HAVE_OPENGL) && defined(HAVE_EGL)
